c3e819062c01387bedacc27e4c51306f92b3ba25
nuradil7704 <113079748+nuradil7704@users.noreply.github.com>
2022-12-03 22:16:07 +0600

    Add files via upload

---
+++ DZ-7_Nuradil_18-01/my-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import About from "./pages/about/About";
+import ButtonPage from "./pages/buttonPage/ButtonPage";
+import Menu from "./component/menu/Menu";
+import MainPage from "./pages/mainPage/MainPage";
+import UserPage from "./pages/userPage/UserPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+
+function App() {
+  return (
+        <BrowserRouter>
+            <Menu/>
+            <Routes>
+                <Route index element={<About/>}/>
+                <Route path="/button" element={<ButtonPage/>}/>
+                <Route path="/main" element={<MainPage/>}/>
+                <Route path="/user" element={<UserPage/>}/>
+                <Route path="/about-page" element={<AboutPage/>}/>
+            </Routes>
+        </BrowserRouter>
+  );
+}
+
+export default App;
